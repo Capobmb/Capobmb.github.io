3879be3487aad2f1d7c1a332f4ce12a46577e327
Capobmb <82255007+Capobmb@users.noreply.github.com>
2024-01-06 19:05:26 +0900

    ✨ Add footer

---
+++ src/components/Footer/footer.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Container from "../Container/container";
+export default function Footer() {
+  return (
+    <footer style={{ margin: "50px auto" }}>
+      <Container>
+        <hr />
+        <h2>Contact me</h2>
+        <ul>
+          <li>
+            <Link href="https://https://github.com/Capobmb">
+              Github: @Capobmb
+            </Link>
+          </li>
+          <li>
+            <Link href="https://twitter.com/cpbm_p">Twitter: @cpbm_p</Link>
+          </li>
+          <li>E-mail: taip0321.h(^^)gmail.com (Replace (^^) with @)</li>
+        </ul>
+        <p>This site is built by Next.js with MDX on Github Pages.</p>
+      </Container>
+    </footer>
+  );
+}
--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -1,9 +1,11 @@
 import Header from "../Header/header";
+import Footer from "../Footer/footer";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
       <main>{children}</main>
+      <Footer />
     </>
   );
 }
